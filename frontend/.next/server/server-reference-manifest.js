be322f78e143726ae1879be829d6c382dc3ac61f
Victoire Schubert <victoire.schubert@gmail.com>
2025-07-01 18:53:20 +0200

    fix(git): update .gitignore

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CBAjetsENu4mpanbjrP6+hkgL+ybO0m8eg45W6f5pXk=\"\n}"
--- frontend/.next/static/chunks/src_app_login_page_tsx_a89e8585._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_899f5e82._.js"
-],
-    source: "dynamic"
-});
